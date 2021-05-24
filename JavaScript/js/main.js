f1234d214c1fc022650efeea1d5c875fa1d2c1e7
callseven <callseven@me.com>
2021-05-23 23:37:42 -0300

    Estudo JS

---
+++ JavaScript/js/main.js
@@ -0,0 +1,33 @@
+// var lista = ["maça", "pera", "laranja"];
+// lista.push("uva");
+// lista.pop();
+// // console.log(lista.length);
+// // console.log(lista);
+// console.log(lista.reverse());
+// console.log(lista.toString());
+// console.log(lista.join(" - "));
+
+// var fruta = {nome:"maça", cor:"vermelha"}
+// console.log(fruta);
+// console.log(fruta.nome);
+// console.log(fruta.cor);
+// alert(fruta.cor);
+
+var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+
+
+
+// var nome = "Call Seven";
+// var idade = 40;
+// var n1 = 5;
+// var n2 = 3;
+// var frase = "Japão é o melhor time do mundo!";
+// alert(nome + " tem " + idade  + " anos ");
+// alert(n1 + n2);
+// console.log(nome);
+// console.log(n1 + n2);
+// console.log(frase.toLocaleUpperCase());
+// console.log(frase.toLocaleUpperCase());
+// alert(frase.replace("Japão", "Brasil"));
