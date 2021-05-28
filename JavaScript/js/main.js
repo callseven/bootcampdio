c6fb898160381336a92c7bb7a5ea6abee76b9b15
callseven <callseven@me.com>
2021-05-27 22:42:51 -0300

    Novo Projeto

--- JavaScript/js/main.js
+++ JavaScript/js/main.js
@@ -1,3 +1,66 @@
+function botao () {
+    // alert("Obrigado por clicar");
+    document.getElementById("agradecimento").innerHTML = ("Obrigado por clicar!");
+}
+
+function redirecionar () {
+    window.open("www.google.com");
+}
+
+/*function validaIdade (idade){
+    var validar;
+    if(idade >= 18){
+        validar = true
+    }else {
+        validar = false
+    }
+    return validar;
+}
+
+var idade = prompt ("Qual sua idade")
+    console.log(validaIdade(idade));
+*/
+
+/*function soma(n1, n2){
+    return n1 + n2;
+}
+
+function setReplace (frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome)
+}
+alert(soma(5, 10));
+alert(setReplace("Vai Japão", "Japão", "Brasil"));
+*/
+/* var d = new Date();
+alert(d.getMilliseconds());
+alert(d.getDate());
+alert(d.getMinutes());
+alert(d.getMonth()+1);
+*/
+
+
+/* var count;
+for(count=0; count<=5; count++){
+    alert(count);
+}
+*/
+
+/* var count = 0;
+while (count <5) {
+    console.log(count);
+    alert(count);
+    count = count + 1; //count++
+}
+*/
+
+/* var idade = prompt("Qual sua idade?");
+if (idade >= 18){
+    alert("Maior de idade");
+} else {
+    alert("Menor de idade");
+}
+*/
+
 // var lista = ["maça", "pera", "laranja"];
 // lista.push("uva");
 // lista.pop();
@@ -13,9 +76,9 @@
 // console.log(fruta.cor);
 // alert(fruta.cor);
 
-var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
-console.log(frutas);
-alert(frutas[1].nome);
+// var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+// console.log(frutas);
+// alert(frutas[1].nome);
 
 
 
