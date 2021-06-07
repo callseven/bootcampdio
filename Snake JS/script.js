6684ce14bf3ace9094b3cac94a636f6c169c8120
callseven <callseven@me.com>
2021-06-06 22:46:00 -0300

    bg snake

---
+++ Snake JS/script.js
@@ -0,0 +1,28 @@
+let canvas = document.getElementById("snake");
+let context = canvas.getContext("2d");
+let box = 32;
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
+let direction = "right";
+
+function criarBG(){
+    context.fillStyle = "lightblue";
+    context.fillRect(0, 0, 16 * box, 16 * box);
+}
+
+function criarCobrinha() {
+    for (i = 0; i < snake.length; i++) {
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
+function iniciarJogo (){
+    criarBG();
+    criarCobrinha();
+}
+
+let jogo = setInterval(iniciarJogo, 100);
